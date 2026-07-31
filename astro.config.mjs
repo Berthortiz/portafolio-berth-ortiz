d8cf6f241dc11df356efb94f530c2799792ae433
berth <berthortizweb@gmail.com>
2026-07-31 14:54:31 -0600

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-
 import icon from "astro-icon";
+import { imagetools } from "vite-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
+
+  output: 'static',
+  site: 'https://berth-ortiz.pages.dev',
   vite: {
-      plugins: [tailwindcss()],
+    plugins: [tailwindcss(), imagetools()],
   },
 
   integrations: [icon()],
